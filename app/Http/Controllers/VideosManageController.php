57db56b0f2bca2ca914a5eeb5939d461e1c7e58d
«sara21h» <sbogdan@iesebre.com>
2024-05-31 01:29:35 +0200

    select de serie al video

--- app/Http/Controllers/VideosManageController.php
+++ app/Http/Controllers/VideosManageController.php
@@ -2,78 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideosManageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return view ('videos.manage.index',[
-            'videos' => Video::all()
+        return view('videos.manage.index', [
+            'videos' => Video::all(),
+            'series' => Serie::all(),
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
+        // Validate form data
         $request->validate([
             'title' => 'required|string',
             'description' => 'required|string',
             'url' => 'required|url',
+            'serie_id' => 'required|integer',
         ]);
 
-        $url = $request->input('url');
-
-        $embed_url = $this->convertirAEmbed($url);
-
-
+        // Create a new video
         $video = Video::create([
-            'title' => $request->input('title'),
-            'description' => $request->input('description'),
-            'url' => $embed_url,
+            'title' => $request->title,
+            'description' => $request->description,
+            'url' => $request->url,
+            'serie_id' => $request->serie_id,
         ]);
-        session()->flash('success', 'Video creat correctament');
+
+        // Redirect with success message
+        session()->flash('success', 'Video creado correctamente');
         return redirect()->route('manage.videos');
     }
 
-    private function convertirAEmbed($url)
-    {
-        $pattern = '/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/';
-
-        preg_match($pattern, $url, $matches);
-
-        if (isset($matches[1])) {
-            return 'https://www.youtube.com/embed/' . $matches[1];
-        } else {
-            return $url;
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         $video = Video::findOrFail($id);
@@ -82,35 +57,33 @@ class VideosManageController extends Controller
 
     public function update(Request $request, $id)
     {
+        // Validate form data
         $request->validate([
             'title' => 'required|string',
             'description' => 'required|string',
             'url' => 'required|url',
+            'serie_id' => 'required|integer',
         ]);
 
         $video = Video::findOrFail($id);
 
-        $url = $request->input('url');
-        $embed_url = $this->convertirAEmbed($url);
-
+        // Update video
         $video->update([
-            'title' => $request->input('title'),
-            'description' => $request->input('description'),
-            'url' => $embed_url,
+            'title' => $request->title,
+            'description' => $request->description,
+            'url' => $request->url,
+            'serie_id' => $request->serie_id,
         ]);
 
-        session()->flash('success', 'Video editat correctament');
+        // Redirect with success message
+        session()->flash('success', 'Video actualizado correctamente');
         return redirect()->route('manage.videos');
     }
 
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
-        Video::find($id)->delete();
-        session()->flash('deleted', 'Video borrat correctament');
+        Video::findOrFail($id)->delete();
+        session()->flash('deleted', 'Video borrado correctamente');
         return redirect()->route('manage.videos');
     }
 }
--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -25,7 +25,7 @@
                     </script>
                 @endif
 
-                <h2 class="mb-4 text-center text-xl tracking-wide">{{ isset($video) ? 'Editar vídeo' : 'Crear vídeo' }}</h2>
+                <h2 class="mb-4 text-center text-xl tracking-wide">{{ isset($video) ? 'Editar video' : 'Crear video' }}</h2>
                 <div class="mb-12 border shadow p-6 rounded-lg">
                     @can('videos_manage_create')
                         <form id="form" class="grid grid-cols-1 gap-y-4 justify-center" data-qa="form_video_create"
@@ -35,31 +35,42 @@
                             @if(isset($video))
                                 @method('PUT')
                             @endif
-                            <label class="tracking-wide" for="title">Título</label>
+                            <label class="tracking-wide" for="title">Títol</label>
                             <input class="rounded-lg text-gray-500 text-sm" style="border: none; --tw-ring-color: #45B39D" id="title" name="title" type="text" value="{{ isset($video) ? $video->title : '' }}">
-                            <label class="tracking-wide" for="description">Descripción</label>
-                            <textarea class="rounded-lg text-gray-500 text-sm" style="border: none; --tw-ring-color: #45B39D" name="description" id="description" cols="30" rows="5">{{ isset($video) ? $video->description : '' }}</textarea>
+                            <label class="tracking-wide" for="description">Descripció</label>
+                            <input class="rounded-lg text-gray-500 text-sm" style="border: none; --tw-ring-color: #45B39D" id="description" name="description" type="text" value="{{ isset($video) ? $video->description : '' }}">
                             <label class="tracking-wide" for="url">URL</label>
                             <input class="rounded-lg text-gray-500 text-sm" style="border: none; --tw-ring-color: #45B39D" id="url" name="url" type="url" value="{{ isset($video) ? $video->url : '' }}">
-                            <button id="submitButton" class="bg-white rounded-lg py-1 my-2 mx-64 text-sm font-light shadow" style="color: #566573; outline: none" type="button">{{ isset($video) ? 'Actualizar' : 'Crear' }}</button>
+                            <label class="tracking-wide mt-4" for="series">Selecciona una serie:</label>
+                            <select id="series" name="serie_id" class="block w-full mt-1 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
+                                @if($series->isEmpty())
+                                    <option value="" disabled>No hi ha ninguna serie</option>
+                                @else
+                                    @foreach($series as $serie)
+                                        <option value="{{ $serie->id }}">{{ $serie->nom }}</option>
+                                    @endforeach
+                                @endif
+                            </select>
+                            <button id="submitButton" class="bg-white rounded-lg py-1 my-2 mx-64 text-sm font-light shadow" style="color: #566573; outline: none" type="submit">{{ isset($video) ? 'Actualizar' : 'Crear' }}</button>
                         </form>
                     @endcan
                 </div>
-                    <script>
-                        function updateVideo() {
-                            document.getElementById('form').submit();
-                        }
-                    </script>
+                <script>
+                    function updateVideo() {
+                        document.getElementById('form').submit();
+                    }
+                </script>
 
-                <h2 class="mb-4 text-center text-xl tracking-wide">Llistat dels videos</h2>
+                <h2 class="mb-4 text-center text-xl tracking-wide">Lista de videos</h2>
                 <div class="overflow-y-auto" style="max-height: 500px;">
                     <table class="min-w-full">
                         <thead class="bg-gray-200 border-b">
                         <tr>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Id</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Title</th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">ID</th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Títol</th>
                             <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Descripció</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-2 py-4 text-left">URL</th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">URL</th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Serie id</th>
                             <th scope="col" class="text-sm font-medium text-gray-900 px-4 py-4 text-left">Accions</th>
                         </tr>
                         </thead>
@@ -67,9 +78,12 @@
                         @foreach($videos as $video)
                             <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 cursor-pointer">
                                 <td class="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $video->id }}</td>
-                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap" onclick="redirectToVideo('{{ $video->id }}')">{{ $video->title }}</td>
-                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap" onclick="redirectToVideo('{{ $video->id }}')">{{ $video->description }}</td>
-                                <td class="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap overflow-hidden text-ellipsis" style="max-width: 150px;">{{ $video->url }}</td>
+                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
+                                    <a href="/videos/{{ $video->id }}">{{ $video->title }}</a>
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">{{ $video->description }}</td>
+                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">{{ $video->url }}</td>
+                                <td class="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">{{ $video->serie_id }}</td>
                                 <td class="relative px-4 py-4">
                                     <div class="flex space-x-2">
                                         <form action="{{ route('videos.destroy', $video->id) }}" method="POST">
@@ -79,26 +93,27 @@
                                         </form>
                                         <button style="outline: none" class="border p-2 rounded-lg bg-gray-200 hover:bg-red-50 hover:text-gray-600" onclick="openEditModal(event, {{ $video }})">Editar</button>
                                         <script>
-                                            function redirectToVideo(videoId) {
-                                                window.location.href = `/videos/${videoId}`;
-                                            }
-                                        </script>
-                                        <script>
                                             function openEditModal(event, video) {
-                                                event.preventDefault(); // Detener la acción predeterminada del enlace
-                                                const modal = document.getElementById('modal');
+                                                event.preventDefault();
                                                 const title = document.getElementById('title');
                                                 const description = document.getElementById('description');
                                                 const url = document.getElementById('url');
+                                                const serie_id = document.getElementById('serie_id');
                                                 const form = document.getElementById('form');
                                                 const submitButton = document.getElementById('submitButton');
+
+                                                // Set form values
                                                 title.value = video.title;
                                                 description.value = video.description;
                                                 url.value = video.url;
+                                                serie_id.value = video.serie_id;
+
+                                                // Set form action to the update route
                                                 form.action = `/videos/${video.id}`;
-                                                form.method = 'POST';
-                                                submitButton.innerText = 'Actualizar';
-                                                submitButton.onclick = function() {
+
+                                                // Include the method spoofing input
+                                                submitButton.onclick = function()
+                                                {
                                                     form.method = 'POST';
                                                     const input = document.createElement('input');
                                                     input.setAttribute('type', 'hidden');
@@ -107,7 +122,9 @@
                                                     form.appendChild(input);
                                                     form.submit();
                                                 };
-                                                modal.style.display = 'block';
+
+                                                // Change submit button text
+                                                submitButton.innerText = 'Actualizar';
                                             }
                                         </script>
                                     </div>
